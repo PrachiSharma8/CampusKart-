9bba8e93bc40669fb0d6bcd0238f5d00e3bd073f
Prachi Sharma <prachisharma4674@gmail.com>
2026-07-15 22:35:28 +0530

    updates allowed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,8 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// const auth = require("../middleware/auth");
-// const upload = require("../config/multer");
+const auth = require("../middleware/auth");
+const upload = require("../config/multer");
 
 // const {
 //     addProduct,
